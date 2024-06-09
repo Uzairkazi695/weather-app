8f6257f69a28204e91498c162b889066534d84c4
Uzairkazi695 <167708998+Uzairkazi695@users.noreply.github.com>
2024-06-09 23:56:28 +0530

    Added a alt attribute in img tag

--- script.js
+++ script.js
@@ -20,20 +20,28 @@ search.addEventListener('click', ()=>{
         windSpeed.textContent = `${data.wind.speed}km/h`
         if(data.weather[0].main === 'Clouds') {
             icon.src = '/images/clouds.png'
+            icon.alt = 'Cloudy'
         } else if(data.weather[0].main === 'Clear') {
             icon.src = '/images/clear.png'
+            icon.alt = 'Clear'
         } else if(data.weather[0].main === 'Drizzle') {
             icon.src = '/images/drizzle.png'
+            icon.alt = 'Drizzle'
         } else if(data.weather[0].main === 'Haze') {
             icon.src = '/images/weather.png'
+            icon.alt = 'Haze'
         } else if(data.weather[0].main === 'Mist') {
             icon.src = '/images/mist.png'
+            icon.alt = 'Mist'
         } else if(data.weather[0].main === 'Rain') {
             icon.src = '/images/rain.png'
+            icon.alt = 'Rain'
         } else if(data.weather[0].main === 'Snow') {
             icon.src = '/images/snow.png'
+            icon.alt = 'Snow'
         } else if(data.weather[0].main === 'Wind') {
             icon.src = '/images/wind.png'
+            icon.alt = 'Wind'
         }
     })
 })
